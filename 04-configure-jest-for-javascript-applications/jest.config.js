03e3415dfe55f1966a7d19c5fe1b0db38ee1e4e7
R Sánchez <rene.as.himself@gmail.com>
2020-04-22 18:31:13 -0500

    07 Custom Jest snapshots for Emotion

--- 04-configure-jest-for-javascript-applications/jest.config.js
+++ 04-configure-jest-for-javascript-applications/jest.config.js
@@ -7,7 +7,14 @@ module.exports = {
   moduleNameMapper: {
     // To fix the error from trying to import CSS files, configure Jest to resolve to a file we specify whenever if it ends with .css
     '\\.css$': require.resolve('./test/style-mock.js'),
-    // 
+    //
     '\\.module\\.css$': 'identity-obj-proxy',
   },
+
+  // Use snapshot serializers like jest-emotion
+  snapshotSerializers: [
+    // Replaces absolute paths with normalized paths. Useful when devs have different paths on their systems and there are differences in all snapshots.
+    // 'jest-serializer-path',
+    'jest-emotion'
+  ],
 };

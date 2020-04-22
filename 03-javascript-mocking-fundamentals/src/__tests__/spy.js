6d4d00bf2d5cb751696bc15a1f7fc94f93b2d9d3
R Sánchez <rene.as.himself@gmail.com>
2020-04-22 16:58:17 -0500

    Implementing Jest's spyOn

--- 03-javascript-mocking-fundamentals/src/__tests__/spy.js
+++ 03-javascript-mocking-fundamentals/src/__tests__/spy.js
@@ -2,7 +2,9 @@ const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
 test('returns winner', () => {
+  // spyOn replaces the function passed with an empty mock function
   jest.spyOn(utils, 'getWinner')
+  // here we pass an implementation to the mock function (because it was empty)
   utils.getWinner.mockImplementation((p1, p2) => p1)
 
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')

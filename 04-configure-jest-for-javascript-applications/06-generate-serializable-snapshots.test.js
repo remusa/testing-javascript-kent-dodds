c40f3b4e0d5555c86e3b6f05ba6c73e0287ce0af
R Sánchez <rene.as.himself@gmail.com>
2020-04-22 18:24:40 -0500

    06 Generate serializable snapshots

---
+++ 04-configure-jest-for-javascript-applications/06-generate-serializable-snapshots.test.js
@@ -0,0 +1,26 @@
+import '@testing-library/react'
+import React from 'react'
+import {render} from '@testing-library/react'
+import CalculatorDisplay from './src/calculator-display'
+import {getFlyingSuperHeros} from './src/super-heros'
+
+describe('super heros', () => {
+  test('returns super heros that can fly', () => {
+    const flyingHeros = getFlyingSuperHeros()
+
+    expect(flyingHeros).toMatchSnapshot()
+  })
+})
+
+describe('CalculatorDisplay', () => {
+  test('mounts', () => {
+    const {container} = render(<CalculatorDisplay value="0" />)
+
+    // RTL's render function returns its container property wrapping the component in a div
+    // The rendered component can be obtained using container.firstChild
+    console.log(container.innerHTML)
+    console.log(container.firstChild.innerHTML)
+
+    expect(container.firstChild.innerHTML).toMatchSnapshot()
+  })
+})
